0075de5f4e2af0d5974c101bc1c9642d50fc8164
PedrorFelix <pedrojrfelix1999@gmail.com>
2025-02-24 16:41:43 +0000

    changes to main method

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+import sys
 
 def main():
     parser = argparse.ArgumentParser(description="Replicates a source folder frequently")
@@ -7,6 +9,19 @@ def main():
     parser.add_argument("-l", "--log", help="Path of the log file", required=True)
     parser.add_argument("-f", "--frequency", help="frequency with witch the replication must occur", required=True)
     args = parser.parse_args()
+
+
+    if os.path.exists(args.source):
+        print("Source exists")
+        if os.path.exists(args.replica):
+            print("Replica exists")
+            return 0
+        else:
+            print("Replica to be created")
+            return 0
+    else:
+        print("Source does not exist")
+        sys.exit(1)
 
     print(f"arguments:\nsource: {args.source},\nreplica: {args.replica},\nlog: {args.log},\nfrequency: {args.frequency}")
     return 0
