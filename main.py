c4eb1fd989d427b4f66f9f017399a6f2e1fe110a
PedrorFelix <pedrojrfelix1999@gmail.com>
2025-02-24 16:26:37 +0000

    basic argument parser implmentation

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import argparse
+
 def main():
-    print("code still to be implemented")
+    parser = argparse.ArgumentParser(description="Replicates a source folder frequently")
+    parser.add_argument("-s", "--source", help="Path of the folder to be replicated", required=True)
+    parser.add_argument("-r", "--replica", help="Path of the replica folder", required=True)
+    parser.add_argument("-l", "--log", help="Path of the log file", required=True)
+    parser.add_argument("-f", "--frequency", help="frequency with witch the replication must occur", required=True)
+    args = parser.parse_args()
+
+    print(f"arguments:\nsource: {args.source},\nreplica: {args.replica},\nlog: {args.log},\nfrequency: {args.frequency}")
     return 0
 
 if __name__ == "__main__":
