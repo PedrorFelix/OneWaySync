9c3868e83363c25b1c2ae92a6fcb8b5b15bdc3e5
PedrorFelix <pedrojrfelix1999@gmail.com>
2025-02-25 17:02:30 +0000

    loop and log verification

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+import time
 
 def main():
     parser = argparse.ArgumentParser(description="Replicates a source folder frequently")
@@ -10,21 +11,35 @@ def main():
     parser.add_argument("-f", "--frequency", help="frequency with witch the replication must occur", required=True)
     args = parser.parse_args()
 
+    print(f"arguments:\nsource: {args.source},\nreplica: {args.replica},\nlog: {args.log},\nfrequency: {args.frequency}")
 
-    if os.path.exists(args.source):
-        print("Source exists")
-        if os.path.exists(args.replica):
-            print("Replica exists")
-            return 0
-        else:
-            print("Replica to be created")
-            return 0
-    else:
-        print("Source does not exist")
+    try:
+        frequency = int(args.frequency)
+    except ValueError:
+        print("Invalid Frequency (Must be a numeric value)")
         sys.exit(1)
 
-    print(f"arguments:\nsource: {args.source},\nreplica: {args.replica},\nlog: {args.log},\nfrequency: {args.frequency}")
-    return 0
+    if os.path.exists(args.source):
+        
+        if os.path.exists(args.replica):
+            
+            return 0
+        else:
+            print("Replica folder does not exist, new folder will be created") #log this
+            return 0
+    else:
+        print("Source folder does not exist")
+        sys.exit(1)
+
+    try:
+        while true:
+            print("Running the Script. Press Ctrl+C to stop")
+            #run sync
+            time.sleep(frequency)
+    except KeyboardInterrupt:
+        print("Script Stopped")
+        sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
